f41c7d45832eb79d27e17e056d81087dc640a3a2
EduardoHFBezerra <eduardohfbezerra@gmail.com>
2022-10-27 15:59:00 -0300

    AtividadeController

--- back/src/ProAtividade.API/Controllers/AtividadeController.cs
+++ back/src/ProAtividade.API/Controllers/AtividadeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Models;
 
 namespace ProAtividade.API.Controllers
 {
@@ -10,6 +11,34 @@ namespace ProAtividade.API.Controllers
     [Route("api/[controller]")]
     public class AtividadeController : ControllerBase
     { 
-        
+        public IEnumerable<Atividade> Atividades = new List<Atividade>() {
+                new Atividade(1),
+                new Atividade(2),
+                new Atividade(3)
+            };
+        [HttpGet]
+        public IEnumerable<Atividade> Get() {
+            return Atividades;
+        }
+
+        [HttpGet("{id}")]
+        public Atividade Get(int id) {
+            return Atividades.FirstOrDefault(ati => ati.Id ==id);
+        }
+
+        [HttpPost]
+        public IEnumerable<Atividade> Post(Atividade atividade) {
+            return Atividades.Append<Atividade>(atividade);
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id) {
+            return "Meu Primeiro Método Put";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id) {
+            return "Meu Primeiro Método Delete";
+        }
     }
 }
